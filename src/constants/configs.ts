d27e326a97cf88c719781e254bc0d3a845d13a45
Sebastian Chitiva <manuchitivar@gmail.com>
2025-12-18 17:28:04 -0500

    ajuste de nombre de la web y discord

--- src/constants/configs.ts
+++ src/constants/configs.ts
@@ -1,14 +1,16 @@
 import { SERVER_LOGO, SERVER_NAME } from "@/configs/configs";
 
-export const webProps ={
+export const webProps = {
   // The name of the server || Nombre del servidor tomado de la configuración del environment
-  serverName: SERVER_NAME, 
+  serverName: SERVER_NAME,
   // The logo of the server || El logo del servidor tomado de la configuración del environment
-  logo: SERVER_LOGO, 
+  logo: SERVER_LOGO,
   // The login banner image || La imagen del banner de inicio de sesión
-  loginBanner: "https://static.wixstatic.com/media/5dd8a0_36549a48c1674682a9c8c8264decd5a2~mv2.webp", 
+  loginBanner:
+    "https://static.wixstatic.com/media/5dd8a0_36549a48c1674682a9c8c8264decd5a2~mv2.webp",
   // The Discord widget URL || La URL del widget de Discord
-  discordWidget: "https://discord.com/widget?id=1375949102818398461&theme=dark", 
+  discordWidget: "https://discord.com/widget?id=1444554820340944969&theme=dark",
   // The features server image || La imagen que se muestra en la sección de noticias del servidor
-  homeFeaturesImg: "https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3M3NneXRqcDl5d3E2emZteGtxaGFoaTliazUybjgyeXhhaDEyczB2ZCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/lp1jZYqFnsAMcI5axO/giphy.gif" 
+  homeFeaturesImg:
+    "https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3M3NneXRqcDl5d3E2emZteGtxaGFoaTliazUybjgyeXhhaDEyczB2ZCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/lp1jZYqFnsAMcI5axO/giphy.gif",
 };
