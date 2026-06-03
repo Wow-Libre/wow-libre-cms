432182b9ecd4faf614095e6a47e61f980589d59b
Sebastian Chitiva <manuchitivar@gmail.com>
2026-06-03 10:30:49 -0500

    Mejoro la vista de contribuciones

---
+++ src/features/contributions/constants.ts
@@ -0,0 +1,13 @@
+export const CONTRIBUTIONS_DECORATIVE_TREANT =
+  "https://static.wixstatic.com/media/5dd8a0_a1d175976a834a9aa2db34adb6d87d02~mv2.png";
+
+export const CONTRIBUTIONS_HERO_IMAGES = [
+  {
+    src: "https://static.wixstatic.com/media/5dd8a0_423a9b289cbb4211aeec71a2ca145926~mv2.webp",
+    alt: "Aventureros en Azeroth",
+  },
+  {
+    src: "https://static.wixstatic.com/media/5dd8a0_7541713497634f41807acf80546c561c~mv2.webp",
+    alt: "Reino de Azeroth",
+  },
+] as const;
---
+++ src/features/contributions/index.ts
@@ -0,0 +1,3 @@
+export { ContributionsClientsSection } from "./components/ContributionsClientsSection";
+export { ContributionsHero } from "./components/ContributionsHero";
+export { CONTRIBUTIONS_DECORATIVE_TREANT } from "./constants";
